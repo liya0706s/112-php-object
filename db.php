95169eb708dacbb079f48b9c3a8967deadc93983
Angie <liya0706s@gmail.com>
2023-11-24 14:35:21 +0800

    修正db函式

--- db.php
+++ db.php
@@ -2,31 +2,36 @@
 date_default_timezone_set("Asia/Taipei");
 session_start();
 
+// 定義一個 DB 類別
 class DB
 {
-    protected $dsn = "mysql:host=localhost;charset=utf8;dbname=school";
+    // 資料庫連接字串，指定主機、編碼和資料庫名稱（需要更改 dbname）
     // dbname needs to be changed
-    protected $pdo;
+    protected $dsn = "mysql:host=localhost;charset=utf8;dbname=school";
     // 宣告$pdo不能有運算元
+    // 資料庫連接實例
+    protected $pdo;
     protected $table;
-    // 增加新參數
 
+    // 建構函式，初始化 DB 物件時執行，接收一個資料表名稱參數
     public function __construct($table)
     {
+        // 將傳入的資料表名稱存儲在 $this->table 中
         $this->table = $table;
-        $this->pdo = new PDO($this->dsn, 'root', '');
+
+        // 創建 PDO 連接實例，連接資料庫
         // $this->dsn讀外面的$dsn
+        $this->pdo = new PDO($this->dsn, 'root', '');
     }
-    // 以上，建立建構式
+    // 以上，資料庫操作建立建構式
 
 
-
-    function all($table = null, $where = '', $other = '')
+    
+    function all($where = '', $other = '')
     {
-        global $pdo;
-        $sql = "select * from `$table` ";
+        $sql = "select * from `$this->table` ";
 
-        if (isset($table) && !empty($table)) {
+        if (isset($this->table) && !empty($this->table)) {
 
             if (is_array($where)) {
 
@@ -42,18 +47,16 @@ class DB
 
             $sql .= $other;
             //echo 'all=>'.$sql;
-            $rows = $pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
+            $rows = $this->pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
             return $rows;
         } else {
             echo "錯誤:沒有指定的資料表名稱";
         }
     }
 
-
-    function find($table, $id)
+    function find($id)
     {
-        global $pdo;
-        $sql = "select * from `$table` ";
+        $sql = "select * from `$this->table` ";
 
         if (is_array($id)) {
             foreach ($id as $col => $value) {
@@ -66,15 +69,14 @@ class DB
             echo "錯誤:參數的資料型態比須是數字或陣列";
         }
         //echo 'find=>'.$sql;
-        $row = $pdo->query($sql)->fetch(PDO::FETCH_ASSOC);
+        $row = $this->pdo->query($sql)->fetch(PDO::FETCH_ASSOC);
         return $row;
     }
 
-    function update($table, $id, $cols)
+    function update($id, $cols)
     {
-        global $pdo;
 
-        $sql = "update `$table` set ";
+        $sql = "update `$this->table` set ";
 
         if (!empty($cols)) {
             foreach ($cols as $col => $value) {
@@ -97,14 +99,13 @@ class DB
             echo "錯誤:參數的資料型態比須是數字或陣列";
         }
         // echo $sql;
-        return $pdo->exec($sql);
+        return $this->pdo->exec($sql);
     }
 
-    function insert($table, $values)
+    function insert($values)
     {
-        global $pdo;
 
-        $sql = "insert into `$table` ";
+        $sql = "insert into `$this->table` ";
         $cols = "(`" . join("`,`", array_keys($values)) . "`)";
         $vals = "('" . join("','", $values) . "')";
 
@@ -112,13 +113,12 @@ class DB
 
         //echo $sql;
 
-        return $pdo->exec($sql);
+        return $this->pdo->exec($sql);
     }
 
-    function del($table, $id)
+    function del($id)
     {
-        global $pdo;
-        $sql = "delete from `$table` where ";
+        $sql = "delete from `$this->table` where ";
 
         if (is_array($id)) {
             foreach ($id as $col => $value) {
@@ -132,13 +132,19 @@ class DB
         }
         //echo $sql;
 
-        return $pdo->exec($sql);
+        return $this->pdo->exec($sql);
     }
+}
 
-    function dd($array)
+function dd($array)
     {
         echo "<pre>";
         print_r($array);
         echo "</pre>";
     }
-}
+
+$student=new DB('students');
+$rows=$student->find('25');
+print_r($rows);
+
+?>
