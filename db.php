479b6e28278b2e5e20e513ad57263ca2352448e5
Angie <liya0706s@gmail.com>
2023-11-24 14:08:02 +0800

    建立db建構式

--- db.php
+++ db.php
@@ -1,125 +1,144 @@
 <?php
 date_default_timezone_set("Asia/Taipei");
-$dsn = "mysql:host=localhost;charset=utf8;dbname=school";
-$pdo = new PDO($dsn, 'root', '');
+session_start();
 
-function all($table = null, $where = '', $other = '')
+class DB
 {
-    global $pdo;
-    $sql = "select * from `$table` ";
+    protected $dsn = "mysql:host=localhost;charset=utf8;dbname=school";
+    // dbname needs to be changed
+    protected $pdo;
+    // 宣告$pdo不能有運算元
+    protected $table;
+    // 增加新參數
 
-    if (isset($table) && !empty($table)) {
+    public function __construct($table)
+    {
+        $this->table = $table;
+        $this->pdo = new PDO($this->dsn, 'root', '');
+        // $this->dsn讀外面的$dsn
+    }
+    // 以上，建立建構式
 
-        if (is_array($where)) {
 
-            if (!empty($where)) {
-                foreach ($where as $col => $value) {
-                    $tmp[] = "`$col`='$value'";
+
+    function all($table = null, $where = '', $other = '')
+    {
+        global $pdo;
+        $sql = "select * from `$table` ";
+
+        if (isset($table) && !empty($table)) {
+
+            if (is_array($where)) {
+
+                if (!empty($where)) {
+                    foreach ($where as $col => $value) {
+                        $tmp[] = "`$col`='$value'";
+                    }
+                    $sql .= " where " . join(" && ", $tmp);
                 }
-                $sql .= " where " . join(" && ", $tmp);
+            } else {
+                $sql .= " $where";
+            }
+
+            $sql .= $other;
+            //echo 'all=>'.$sql;
+            $rows = $pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
+            return $rows;
+        } else {
+            echo "錯誤:沒有指定的資料表名稱";
+        }
+    }
+
+
+    function find($table, $id)
+    {
+        global $pdo;
+        $sql = "select * from `$table` ";
+
+        if (is_array($id)) {
+            foreach ($id as $col => $value) {
+                $tmp[] = "`$col`='$value'";
+            }
+            $sql .= " where " . join(" && ", $tmp);
+        } else if (is_numeric($id)) {
+            $sql .= " where `id`='$id'";
+        } else {
+            echo "錯誤:參數的資料型態比須是數字或陣列";
+        }
+        //echo 'find=>'.$sql;
+        $row = $pdo->query($sql)->fetch(PDO::FETCH_ASSOC);
+        return $row;
+    }
+
+    function update($table, $id, $cols)
+    {
+        global $pdo;
+
+        $sql = "update `$table` set ";
+
+        if (!empty($cols)) {
+            foreach ($cols as $col => $value) {
+                $tmp[] = "`$col`='$value'";
             }
         } else {
-            $sql .= " $where";
+            echo "錯誤:缺少要編輯的欄位陣列";
         }
 
-        $sql .= $other;
-        //echo 'all=>'.$sql;
-        $rows = $pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
-        return $rows;
-    } else {
-        echo "錯誤:沒有指定的資料表名稱";
-    }
-}
-
-
-function find($table, $id)
-{
-    global $pdo;
-    $sql = "select * from `$table` ";
-
-    if (is_array($id)) {
-        foreach ($id as $col => $value) {
-            $tmp[] = "`$col`='$value'";
+        $sql .= join(",", $tmp);
+        $tmp = [];
+        if (is_array($id)) {
+            foreach ($id as $col => $value) {
+                $tmp[] = "`$col`='$value'";
+            }
+            $sql .= " where " . join(" && ", $tmp);
+        } else if (is_numeric($id)) {
+            $sql .= " where `id`='$id'";
+        } else {
+            echo "錯誤:參數的資料型態比須是數字或陣列";
         }
-        $sql .= " where " . join(" && ", $tmp);
-    } else if (is_numeric($id)) {
-        $sql .= " where `id`='$id'";
-    } else {
-        echo "錯誤:參數的資料型態比須是數字或陣列";
+        // echo $sql;
+        return $pdo->exec($sql);
     }
-    //echo 'find=>'.$sql;
-    $row = $pdo->query($sql)->fetch(PDO::FETCH_ASSOC);
-    return $row;
-}
 
-function update($table, $id, $cols)
-{
-    global $pdo;
+    function insert($table, $values)
+    {
+        global $pdo;
 
-    $sql = "update `$table` set ";
+        $sql = "insert into `$table` ";
+        $cols = "(`" . join("`,`", array_keys($values)) . "`)";
+        $vals = "('" . join("','", $values) . "')";
 
-    if (!empty($cols)) {
-        foreach ($cols as $col => $value) {
-            $tmp[] = "`$col`='$value'";
+        $sql = $sql . $cols . " values " . $vals;
+
+        //echo $sql;
+
+        return $pdo->exec($sql);
+    }
+
+    function del($table, $id)
+    {
+        global $pdo;
+        $sql = "delete from `$table` where ";
+
+        if (is_array($id)) {
+            foreach ($id as $col => $value) {
+                $tmp[] = "`$col`='$value'";
+            }
+            $sql .= join(" && ", $tmp);
+        } else if (is_numeric($id)) {
+            $sql .= " `id`='$id'";
+        } else {
+            echo "錯誤:參數的資料型態比須是數字或陣列";
         }
-    } else {
-        echo "錯誤:缺少要編輯的欄位陣列";
+        //echo $sql;
+
+        return $pdo->exec($sql);
     }
 
-    $sql .= join(",", $tmp);
-    $tmp = [];
-    if (is_array($id)) {
-        foreach ($id as $col => $value) {
-            $tmp[] = "`$col`='$value'";
-        }
-        $sql .= " where " . join(" && ", $tmp);
-    } else if (is_numeric($id)) {
-        $sql .= " where `id`='$id'";
-    } else {
-        echo "錯誤:參數的資料型態比須是數字或陣列";
+    function dd($array)
+    {
+        echo "<pre>";
+        print_r($array);
+        echo "</pre>";
     }
-    // echo $sql;
-    return $pdo->exec($sql);
-}
-
-function insert($table, $values)
-{
-    global $pdo;
-
-    $sql = "insert into `$table` ";
-    $cols = "(`" . join("`,`", array_keys($values)) . "`)";
-    $vals = "('" . join("','", $values) . "')";
-
-    $sql = $sql . $cols . " values " . $vals;
-
-    //echo $sql;
-
-    return $pdo->exec($sql);
-}
-
-function del($table, $id)
-{
-    global $pdo;
-    $sql = "delete from `$table` where ";
-
-    if (is_array($id)) {
-        foreach ($id as $col => $value) {
-            $tmp[] = "`$col`='$value'";
-        }
-        $sql .= join(" && ", $tmp);
-    } else if (is_numeric($id)) {
-        $sql .= " `id`='$id'";
-    } else {
-        echo "錯誤:參數的資料型態比須是數字或陣列";
-    }
-    //echo $sql;
-
-    return $pdo->exec($sql);
-}
-
-function dd($array)
-{
-    echo "<pre>";
-    print_r($array);
-    echo "</pre>";
 }
