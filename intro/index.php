ccee0052c965fbd4d95a0ed49b4665c13af9345a
Angie <liya0706s@gmail.com>
2023-11-26 12:58:01 +0800

    feat:複習class 屬性方法 物件

--- intro/index.php
+++ intro/index.php
@@ -1,27 +1,30 @@
 <h1>物件介紹</h1>
 <?php
 
-// // 定義一個 Animal 類別
+// 類別
 class Animal{
-     // 在類別內 定義公開的 屬性
+     // 在類別內 定義公開的變數稱為，屬性
     public $name;
 
-    // 定義一個公開的 方法 setName函式，用來設定動物的名稱
+    // 在類別內 定義一個公開的函式稱為，方法 
+    // setName函式，用來設定動物的名稱
     public function setName($name){
         // 使用 $this->name 存儲傳入的名稱
-        // 把$name指定回等號左邊
+        // 將傳入的名稱設定給物件的 name 屬性
         $this->name=$name;
     }
 }
 
+// 類別 實例化 instant 又稱 實體化 new..，
 // 建立物件$animal= new class_name
 $animal=new Animal; 
-//實例化 instant 又稱 實體化 new..，
+
 
 // 輸出目前動物的名稱，注意此時 $animal->name 尚未設定，因此顯示空字串
 echo '顯示名稱:'.$animal->name;
 // name預設是沒有值
 echo "<br>";
+
 // 呼叫 Animal 物件的 setName 方法，設定動物的名稱為 '小花'
 $animal->setName('小花');
 // 再次輸出動物的名稱，此時應該顯示 '小花'
