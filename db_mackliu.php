1a13f1c4489bcde6581903f4eab4817a0af3ffc0
Angie <liya0706s@gmail.com>
2023-11-30 10:15:00 +0800

    抽離sql_all的語法為獨立的內部函式,增加math()為聚合函數的共用函式

--- db_mackliu.php
+++ db_mackliu.php
@@ -1,116 +1,90 @@
-<?php
+<?php 
 date_default_timezone_set("Asia/Taipei");
 session_start();
+class DB{
 
-// 定義一個 DB 類別
-class DB
-{
     protected $dsn = "mysql:host=localhost;charset=utf8;dbname=school";
     protected $pdo;
     protected $table;
 
-    // 建構函式，初始化 DB 物件時執行，接收一個資料表名稱參數
     public function __construct($table)
     {
-        $this->table = $table;
-        $this->pdo = new PDO($this->dsn, 'root', '');
+        $this->table=$table;
+        $this->pdo=new PDO($this->dsn,'root','');
     }
-    // 以上，資料庫操作建立建構式
 
 
-    // 2023-11-30 分析all()和count()只有sql語法不同，下方判斷式都依樣
-    // 2023-11-30 一般查詢all,count 跟 有運算sum,max,min的查詢分開
-    function all($where = '', $other = '')
+    function all( $where = '', $other = '')
     {
         $sql = "select * from `$this->table` ";
-        $sql = $this->sql_all($sql, $where, $other);
-        return $this->pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
+        $sql =$this->sql_all($sql,$where,$other);
+        return  $this->pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
     }
 
-    // 2023-11-24 count()
-    // 2023-11-30 sum(), max(), min()
-    // count不須指定欄位(2個參數)，sum需要指定欄位(3個參數)。
-    function count($where = '', $other = '')
-    {
+    function count( $where = '', $other = ''){
         $sql = "select count(*) from `$this->table` ";
-        $sql = $this->sql_all($sql, $where, $other);
+        $sql=$this->sql_all($sql,$where,$other);
+        return  $this->pdo->query($sql)->fetchColumn(); 
+    }
+    private function math($math,$col,$array='',$other=''){
+        $sql="select $math(`$col`)  from `$this->table` ";
+        $sql=$this->sql_all($sql,$array,$other);
         return $this->pdo->query($sql)->fetchColumn();
     }
-
-    private function math($math, $col, $array = '', $other = '')
-    {
-        $sql = "select $math(`$col`) from `$this->table` ";
-        $sql = $this->sql_all($sql, $array, $other);
-        return $this->pdo->query($sql)->fetchColumn();
+    function sum($col='', $where = '', $other = ''){
+        return  $this->math('sum',$col,$where,$other);
     }
-
-    function sum($col, $where = '', $other = '')
-    {
-        return $this->math('sum', $col, $where, $other);
-    }
-
-    function max($col, $where = '', $other = '')
-    {
-        return $this->math('max', $col, $where, $other);
-    }
-
-    function min($col, $where = '', $other = '')
-    {
-        return $this->math('min', $col, $where, $other);
-    }
-
-
+    function max($col, $where = '', $other = ''){
+        return  $this->math('max',$col,$where,$other);
+    }  
+    function min($col, $where = '', $other = ''){
+        return  $this->math('min',$col,$where,$other);
+    }  
+    
     function find($id)
     {
         $sql = "select * from `$this->table` ";
-
+    
         if (is_array($id)) {
             $tmp = $this->a2s($id);
             $sql .= " where " . join(" && ", $tmp);
         } else if (is_numeric($id)) {
             $sql .= " where `id`='$id'";
         } else {
-            echo "錯誤:參數的資料型態必須是數字或陣列";
+            echo "錯誤:參數的資料型態比須是數字或陣列";
         }
         //echo 'find=>'.$sql;
         $row = $this->pdo->query($sql)->fetch(PDO::FETCH_ASSOC);
         return $row;
     }
-
-    // 2023-11-24 包含新增和更新update, insert
-    function save($array)
-    {
-        // 判斷是否是存在的id，有的話代表是update,無代表是inseert
-        if (isset($array['id'])) {
-
-            // update程式碼---commit:修改update的參數以縮減程式碼
+    
+    function save($array){
+        if(isset($array['id'])){
             $sql = "update `$this->table` set ";
-
+    
             if (!empty($array)) {
                 $tmp = $this->a2s($array);
             } else {
                 echo "錯誤:缺少要編輯的欄位陣列";
             }
-
+        
             $sql .= join(",", $tmp);
             $sql .= " where `id`='{$array['id']}'";
-        } else {
-            // insert程式碼
-            // $this->insert($array);
+        }else{
             $sql = "insert into `$this->table` ";
             $cols = "(`" . join("`,`", array_keys($array)) . "`)";
             $vals = "('" . join("','", $array) . "')";
-
+        
             $sql = $sql . $cols . " values " . $vals;
         }
+
         return $this->pdo->exec($sql);
     }
-
 
     function del($id)
     {
         $sql = "delete from `$this->table` where ";
-
+    
         if (is_array($id)) {
             $tmp = $this->a2s($id);
             $sql .= join(" && ", $tmp);
@@ -120,49 +94,48 @@ class DB
             echo "錯誤:參數的資料型態比須是數字或陣列";
         }
         //echo $sql;
-
+    
         return $this->pdo->exec($sql);
     }
-
-    // 2023-11-24 sql語法
-    function q($sql)
-    {
+    
+    /**
+     * 可輸入各式SQL語法字串並直接執行
+     */
+    function q($sql){
         return $this->pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
+
     }
-    // $array在這邊
-    private function a2s($array)
-    {
+
+    private function a2s($array){
         foreach ($array as $col => $value) {
             $tmp[] = "`$col`='$value'";
         }
         return $tmp;
     }
 
+    private function sql_all($sql,$array,$other){
 
-    // 2023-11-30 
-    private function sql_all($sql, $array, $other)
-    {
         if (isset($this->table) && !empty($this->table)) {
-
+    
             if (is_array($array)) {
-
+    
                 if (!empty($array)) {
                     $tmp = $this->a2s($array);
-                    $sql .= " array " . join(" && ", $tmp);
+                    $sql .= " where " . join(" && ", $tmp);
                 }
             } else {
                 $sql .= " $array";
             }
-
+    
             $sql .= $other;
-            echo $sql;
-            // echo 'sum =>' . $sql;
+            // echo 'all=>'.$sql;
             // $rows = $this->pdo->query($sql)->fetchColumn();
             return $sql;
         } else {
             echo "錯誤:沒有指定的資料表名稱";
         }
     }
+
 }
 
 function dd($array)
@@ -172,16 +145,19 @@ function dd($array)
     echo "</pre>";
 }
 
-$student = new DB('students');
-$rows = $student->count();
+
+$student=new DB('students');
+$rows=$student->count();
 dd($rows);
 echo "<hr>";
-$Score = new DB('student_scores');
-$sum = $Score->sum('score');
+$Score=new DB('student_scores');
+$sum=$Score->sum('score');
 dd($sum);
 echo "<hr>";
-$sum = $Score->sum('score', " where `school_num` <= '911020'");
+$sum=$Score->sum('score'," where `school_num` <= '911020'");
 dd($sum);
 echo "<hr>";
-$sum = $Score -> max('score');
+$sum=$Score->max('score');
 dd($sum);
+
+?>
